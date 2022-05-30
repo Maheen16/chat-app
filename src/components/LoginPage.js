6c148fa76fd95c68b5369014d24dec5fc9f36ec8
Maheen <maheen66005@gmail.com>
2022-05-30 18:22:41 +0530

    login page validation done using formik and yup

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,143 +11,75 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 export default function LoginPage({ snackBarOpen }) {
   //---- states ----
-  const [user, setUser] = useState("");
-  const [roomId, setRoomId] = useState("");
-  const [errorName, setErrorName] = useState(false);
-  const [errorId, setErrorId] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
-  const [checked, setChecked] = useState(false);
-  const [showRoomIdField, setshowRoomIdField] = useState(true);
 
-  //---- hooks ----
   const navigate = useNavigate();
+  const initialValues = { username: "", roomId: "", checked: false };
 
-  // ---- username change ----
-  const handleNameChange = (e) => {
-    setUser(e.target.value);
-    if (e.target.value === "") {
-      setErrorName(true);
+  // Valiating fields
+  let validationSchema = yup.object().shape({
+    checked: yup.boolean(),
+    username: yup.string().required("cannot be empty"),
+    roomId: yup.string().when("checked", {
+      is: (checked) => checked !== true,
+      then: yup
+        .string()
+        .length(4, "only 4 characters")
+        .required("cannot be empty"),
+    }),
+  });
+
+  //---- login  ----
+  const onSubmit = (values) => {
+    console.log(values.checked, "outside if");
+    if (values.checked === true) {
+      console.log(values.checked, "inside if");
+      console.log("values", values);
+      setIsLoading(false);
+      localStorage.setItem("username", values.username);
+      axios
+        .get(`http://192.168.1.89:8000/user/${values.username}`)
+        .then((res) => {
+          console.log(res.data);
+          if (!res.isExist) {
+            navigate(`/rooms/${values.username}`);
+          }
+          // console.log(res.data);
+        })
+        .catch((err) => {
+          const { message } = err.response.data;
+          snackBarOpen(message);
+        });
     } else {
-      setErrorName(false);
-    }
-  };
-
-  // controll roomId Field
-  const handleChange = (event) => {
-    let { checked } = event.target;
-    // console.log(event.target.checked);
-    setChecked(checked);
-    if (checked) {
-      setshowRoomIdField(false);
-    } else {
-      setshowRoomIdField(true);
-    }
-  };
-  // ---- room id handler ----
-  const handleIdChange = (e) => {
-    let error = false;
-    let errorMessage = "";
-    let alphaExp = /^[a-zA-Z]+$/;
-    setRoomId(e.target.value);
-    if (e.target.value === "") {
-      error = true;
-      errorMessage = "room id is required";
-    } else {
-      if (!alphaExp.test(e.target.value)) {
-        errorMessage = "number is not valid";
-        error = true;
-        // console.log("number is not valid");
-      } else {
-        if (e.target.value.length !== 4) {
-          error = true;
-          errorMessage = "only 4 characters";
-        } else {
-          error = false;
-          errorMessage = "";
-        }
-      }
-    }
-    setErrorId(error);
-    setErrorMsg(errorMessage);
-  };
-
-  //---- login with validations ----
-  const logIn = (e) => {
-    e.preventDefault();
-    let flag = false;
-    let alphaExp = /^[a-zA-Z]+$/;
-
-    if (user === "") {
-      setErrorName(true);
-      flag = true;
-    }
-
-    if (showRoomIdField) {
-      if (roomId === "") {
-        setErrorMsg("room id is required");
-        setErrorId(true);
-        flag = true;
-      } else {
-        if (!alphaExp.test(roomId)) {
-          flag = true;
-          // console.log(roomId, "number is not valid");
-        }
-        if (roomId.length !== 4) {
-          flag = true;
-        }
-      }
-    }
-    if (!flag) {
-      setIsLoading(true);
-
-      if (!showRoomIdField) {
-        setIsLoading(false);
-        localStorage.setItem("username", user);
-        axios
-          .get(`http://192.168.1.89:8000/user/${user}`)
-          .then((res) => {
-            if (!res.isExist) {
-              navigate(`/rooms/${user}`);
-            }
-            // console.log(res.data);
-          })
-          .catch((err) => {
-            const { message } = err.response.data;
-            snackBarOpen(message);
-            // user exist logic goes here by API
-            // console.log(err.response.data);
-          });
-      } else {
-        axios
-          .post("http://192.168.1.89:8000/user/login", {
-            username: user,
-            roomId,
-          })
-          .then((res) => {
-            // console.log(res.data);
-            setIsLoading(false);
-            localStorage.setItem("userId", res.data.userId);
-            localStorage.setItem("room_Id", res.data.room_Id);
-            navigate(`/chat/${res.data.room_Id}/${res.data.userId}`);
-          })
-          .catch((err) => {
-            console.log(err);
-            snackBarOpen(err.message);
-            if (err.response.status === 500) {
-              snackBarOpen("Internal Server Error");
-            } else if (err.response.status === 422) {
-              snackBarOpen("you have not signed up.please sign up");
-            } else {
-              snackBarOpen("");
-            }
-            setIsLoading(false);
-          });
-      }
-      // console.log("redirect");
+      axios
+        .post("http://192.168.1.89:8000/user/login", {
+          username: values.username,
+          roomId: values.roomId,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setIsLoading(false);
+          localStorage.setItem("userId", res.data.userId);
+          localStorage.setItem("room_Id", res.data.room_Id);
+          navigate(`/chat/${res.data.room_Id}/${res.data.userId}`);
+        })
+        .catch((err) => {
+          console.log(err);
+          snackBarOpen(err.message);
+          if (err.response.status === 500) {
+            snackBarOpen("Internal Server Error");
+          } else if (err.response.status === 422) {
+            snackBarOpen("you have not signed up.please sign up");
+          } else {
+            snackBarOpen("");
+          }
+          setIsLoading(false);
+        });
     }
   };
 
@@ -184,63 +116,75 @@ export default function LoginPage({ snackBarOpen }) {
           </Typography>
           <Typography variant="body1">Sign In</Typography>
           <Typography variant="body1">Fill Below Information</Typography>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={checked}
-                onChange={handleChange}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              />
-            }
-            label="login with existing roomIds"
-          />
-          <form onSubmit={logIn}>
-            <TextField
-              fullWidth
-              value={user}
-              id="standard-basic"
-              label="Username"
-              size="small"
-              variant="outlined"
-              error={errorName}
-              helperText={errorName && "username is required"}
-              placeholder="Michel Jackson"
-              sx={{ my: 2 }}
-              onChange={handleNameChange}
-            />
-            {showRoomIdField && (
-              <TextField
-                fullWidth
-                value={roomId}
-                error={errorId}
-                id="standard-basic"
-                label="Room Id"
-                helperText={errorMsg}
-                variant="outlined"
-                size="small"
-                placeholder="room id"
-                sx={{ mb: 2 }}
-                onChange={handleIdChange}
-              />
+          <Formik
+            initialValues={initialValues}
+            onChange={({ nextVal }) => setCurrentValues(nextVal)}
+            onSubmit={onSubmit}
+            enableReinitialize={true}
+            validationSchema={validationSchema}
+          >
+            {({ values, errors }) => (
+              <>
+                <Form>
+                  <Field
+                    as={FormControlLabel}
+                    name="checked"
+                    control={
+                      <Switch
+                        checked={values.checked}
+                        sx={{
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                      />
+                    }
+                    label="login with existing roomIds"
+                  />
+                  <Field
+                    as={TextField}
+                    name="username"
+                    fullWidth
+                    id="standard-basic"
+                    label="Username"
+                    size="small"
+                    variant="outlined"
+                    sx={{ my: 2 }}
+                    error={Boolean(errors.username)}
+                    helperText={<ErrorMessage name="username" />}
+                    placeholder="Michel Jackson"
+                  />
+                  {!values.checked && (
+                    <Field
+                      as={TextField}
+                      name="roomId"
+                      fullWidth
+                      id="standard-basic"
+                      label="Room Id"
+                      error={Boolean(errors.roomId)}
+                      helperText={<ErrorMessage name="roomId" />}
+                      variant="outlined"
+                      size="small"
+                      placeholder="room id"
+                      sx={{ mb: 2 }}
+                    />
+                  )}
+                  <Typography variant="body1" sx={{ my: 1 }}>
+                    Start chatting by clicking on :) below button and have fun !
+                  </Typography>
+                  <Button
+                    fullWidth
+                    disabled={isLoading}
+                    variant="contained"
+                    color="primary"
+                    sx={{ mt: 1 }}
+                    type="submit"
+                  >
+                    {isLoading ? <CircularProgress /> : "LogIn"}
+                  </Button>
+                </Form>
+              </>
             )}
-
-            <Typography variant="body1" sx={{ my: 1 }}>
-              Start chatting by clicking on :) below button and have fun !
-            </Typography>
-            <Button
-              fullWidth
-              disabled={isLoading}
-              variant="contained"
-              color="primary"
-              sx={{ mt: 1 }}
-              type="submit"
-            >
-              {isLoading ? <CircularProgress /> : "LogIn"}
-            </Button>
-          </form>
+          </Formik>
           <Box
             sx={{
               display: "flex",
